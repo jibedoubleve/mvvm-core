6cd73c5786ee737ee7e7283e2ad8c3bf6e3ecb7b
jibedoubleve <jibedoubleve@a489d1c4-b3a0-2706-319d-f91dbdd210e5>
2012-06-19 17:09:19 +0000

    * Fix the last commit * Add Empty RelayCommand to ease reading

---
+++ trunk/src/Probel.Mvvm.Core/DataBinding/CommandExtension.cs
@@ -0,0 +1,33 @@
+﻿namespace Probel.Mvvm.DataBinding
+{
+    using System.Windows.Input;
+
+    public static class CommandExtension
+    {
+        #region Methods
+
+        /// <summary>
+        /// Executes if the specified <see cref="ICommand"/> with <c>NULL</c> argument if it can be executed.
+        /// </summary>
+        /// <param name="command">The command to try to execute.</param>
+        public static void TryExecute(this ICommand command)
+        {
+            command.TryExecute(null);
+        }
+
+        /// <summary>
+        /// Executes if the specified <see cref="ICommand"/> with the specified argument if it can be executed.
+        /// </summary>
+        /// <param name="command">The command to try to execute.</param>
+        /// <param name="arg">The arg specified in the command.</param>
+        public static void TryExecute(this ICommand command, object arg)
+        {
+            if (command.CanExecute(arg))
+            {
+                command.Execute(arg);
+            }
+        }
+
+        #endregion Methods
+    }
+}
