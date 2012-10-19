ef406046816e2866b73bbd2d024aba8061ffba72
jibedoubleve <jibedoubleve@a489d1c4-b3a0-2706-319d-f91dbdd210e5>
2012-10-19 15:55:24 +0000

    Add file services to select file in the ViewModel in a seamlessly manner.

---
+++ trunk/src/Probel.Mvvm.Core/Gui/IFileService.cs
@@ -0,0 +1,53 @@
+﻿#region Header
+
+/*
+    This file is part of Mvvm-core.
+
+    Mvvm-core is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Mvvm-core is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Mvvm-core.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion Header
+
+namespace Probel.Mvvm.Gui
+{
+    using System;
+
+    /// <summary>
+    /// Provides an generic way to select file or directories
+    /// </summary>
+    public interface IFileService
+    {
+        #region Methods
+
+        /// <summary>
+        /// Selects the directory.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        void SelectDirectory(Action<string> action);
+
+        /// <summary>
+        /// Selects the file.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        void SelectFile(Action<string> action);
+
+        /// <summary>
+        /// Selects the file where to save the data.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        void SelectFileToSave(Action<string> action);
+
+        #endregion Methods
+    }
+}
