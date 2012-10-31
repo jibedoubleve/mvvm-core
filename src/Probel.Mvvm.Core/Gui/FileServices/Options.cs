7d1b167fe780c124db49160ba97ec77d89ad882b
jibedoubleve <jibedoubleve@a489d1c4-b3a0-2706-319d-f91dbdd210e5>
2012-10-31 23:32:21 +0000

    Add options to SelectFileToSave and SelectFile

---
+++ src/Probel.Mvvm.Core/Gui/FileServices/Options.cs
@@ -0,0 +1,39 @@
+﻿/*
+    This file is part of Mvvm-core.
+
+    Mvvm-core is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Mvvm-core is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Mvvm-core.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Probel.Mvvm.Gui.FileServices
+{
+    public class Options
+    {
+        public Options()
+        {
+            this.Multiselect = false;
+            this.Filter = null;
+            this.InitialDirectory = null;
+            this.Title = null;
+        }
+        public static Options Default
+        {
+            get { return new Options(); }
+        }
+        public string Filter { get; set; }
+        public bool Multiselect { get; set; }
+        public string InitialDirectory { get; set; }
+
+        public string Title { get; set; }
+    }
+}
