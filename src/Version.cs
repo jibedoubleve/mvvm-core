0fa4db6d9572c5212da01a1d96622b1b8d36ffb8
jibedoubleve <jibedoubleve@a489d1c4-b3a0-2706-319d-f91dbdd210e5>
2012-11-08 22:53:14 +0000

    Update for the failed release

--- src/Version.cs
+++ src/Version.cs
@@ -25,4 +25,4 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.0.74")]
+[assembly: AssemblyVersion("1.2.0.76")]
