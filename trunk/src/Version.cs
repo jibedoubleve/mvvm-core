2caf3182a28f21dc38b18b2858a82fbd8c1390fb
jibedoubleve <jibedoubleve@a489d1c4-b3a0-2706-319d-f91dbdd210e5>
2013-02-18 01:50:26 +0000

    Update version file

--- trunk/src/Version.cs
+++ trunk/src/Version.cs
@@ -25,4 +25,4 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("999.999.999")]
+[assembly: AssemblyVersion("999.999.999.999")]
