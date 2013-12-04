7d83b0fe6a3a3e2b5d904287d796c564e5164305
jibedoubleve <jibedoubleve@a489d1c4-b3a0-2706-319d-f91dbdd210e5>
2013-12-04 13:39:35 +0000

    Add helpers Move to .NET 4.5

---
+++ branches/2.0/src/Probel.Mvvm.Test/HelpersTest.cs
@@ -0,0 +1,42 @@
+﻿namespace Probel.Mvvm.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using NUnit.Framework;
+
+    using Probel.Mvvm.Helpers;
+
+    [TestFixture]
+    public class HelpersTest
+    {
+        #region Methods
+
+        [Test]
+        public void Find_NameOfProperty_NameFound()
+        {
+            var name = NameOf<MyClass>.Property(e => e.MyProperty);
+            Assert.AreEqual("MyProperty", name);
+        }
+
+        #endregion Methods
+
+        #region Nested Types
+
+        private class MyClass
+        {
+            #region Properties
+
+            public int MyProperty
+            {
+                get; set;
+            }
+
+            #endregion Properties
+        }
+
+        #endregion Nested Types
+    }
+}
