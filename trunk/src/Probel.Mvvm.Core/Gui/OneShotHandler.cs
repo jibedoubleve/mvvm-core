8e5eee475abe4036e93c6d9b8630c66cfaba6ce7
jibedoubleve <jibedoubleve@a489d1c4-b3a0-2706-319d-f91dbdd210e5>
2013-12-04 19:19:17 +0000

    Reintegrate branche 2.0 into the trunk

---
+++ trunk/src/Probel.Mvvm.Core/Gui/OneShotHandler.cs
@@ -0,0 +1,79 @@
+﻿/*
+    This file is part of Mvvm-core.
+
+    Mvvm-core is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Mvvm-core is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Mvvm-core.  If not, see <http://www.gnu.org/licenses/>.
+*/
+namespace Probel.Mvvm.Gui
+{
+    using System;
+    using System.Reactive;
+    using System.Reactive.Linq;
+    using System.Reflection;
+
+    /// <summary>
+    /// This class handles one shot events
+    /// </summary>
+    /// <typeparam name="TEventSource">The type of the event source.</typeparam>
+    /// <typeparam name="TEventArgs">The type of the event arguments.</typeparam>
+    public class OneShotHandler<TEventSource> : IDisposable
+    {
+        #region Fields
+
+        /// <summary>
+        /// The source of the event
+        /// </summary>
+        protected readonly TEventSource Source;
+
+        private IDisposable Subscription;
+
+        #endregion Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OneShotHandler{TEventSource}" /> class.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        public OneShotHandler(TEventSource source)
+        {
+            this.Source = source;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Subscription.Dispose();
+        }
+
+        /// <summary>
+        /// Handles the specified event when trigered and unsubscribe to it once it was handled.
+        /// </summary>
+        /// <typeparam name="TEventArgs">The type of the event arguments.</typeparam>
+        /// <param name="eventName">Name of the event.</param>
+        /// <param name="handler">The handler which handles the event.</param>
+        public void Handle(string eventName, Action<EventPattern<object>> handler)
+        {
+            var observable = Observable.FromEventPattern(this.Source, eventName);
+            this.Subscription = observable.Subscribe(handler, () => Subscription.Dispose());
+        }
+
+        #endregion Methods
+    }
+}
---
+++ trunk/src/Probel.Mvvm.Test/Helpers/ObservableItem.cs
@@ -0,0 +1,48 @@
+﻿/*
+    This file is part of Mvvm-core.
+
+    Mvvm-core is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Mvvm-core is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Mvvm-core.  If not, see <http://www.gnu.org/licenses/>.
+*/
+namespace Probel.Mvvm.Test.Helpers
+{
+    using Probel.Mvvm.DataBinding;
+
+    public class ObservableItem : ObservableObject
+    {
+        #region Fields
+
+        public const string PropName_TriggerOnLambda = "TriggerOnLambda";
+        public const string PropName_TriggerOnString = "TriggerOnString";
+
+        private string failure = string.Empty;
+        private string triggerOnLambda = string.Empty;
+        private string triggerOnString = string.Empty;
+
+        #endregion Fields
+
+        #region Properties
+
+        public string TriggerOnLambda
+        {
+            get { return this.triggerOnLambda; }
+            set
+            {
+                this.triggerOnLambda = value;
+                this.OnPropertyChanged(() => this.TriggerOnLambda);
+            }
+        }
+
+        #endregion Properties
+    }
+}
