b881e64e2985f1a6d96b43ccc7af22ca462b54cc
jibedoubleve <jibedoubleve@a489d1c4-b3a0-2706-319d-f91dbdd210e5>
2013-12-28 02:51:27 +0000

    Add files in test solution

---
+++ trunk/src/Probel.Mvvm.Test.Gui/Helpers/SimpleBook.cs
@@ -0,0 +1,62 @@
+﻿using Probel.Mvvm.DataBinding;
+/*
+    This file is part of Mvvm-core.
+
+    Mvvm-core is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Mvvm-core is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Mvvm-core.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Probel.Mvvm.Test.Gui.Helpers
+{
+    public class SimpleBookDto : ObservableObject
+    {
+        #region Fields
+
+        private int pages = 0;
+        private string title = string.Empty;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public SimpleBookDto()
+        {
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public int Pages
+        {
+            get { return this.pages; }
+            set
+            {
+                this.pages = value;
+                this.OnPropertyChanged(() => this.Pages);
+            }
+        }
+
+        public string Title
+        {
+            get { return this.title; }
+            set
+            {
+                this.title = value;
+                this.OnPropertyChanged(() => this.Title);
+            }
+        }
+
+        #endregion Properties
+    }
+}
